b7c68a7bd01da981695395af80fcab8aa4b085e5
Hillary Skye <skyeinc9@gmail.com>
2016-02-13 00:53:46 -0700

    Added publications app

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,17 +2,20 @@ app.controller("PortfolioCtrl", ["$scope", function($scope) {
   console.log('from controller')
 
   console.log('two')
+
   $scope.projects = [
     {
-      name: 'tuneSet',
-      url: 'https://stormy-waters-7608.herokuapp.com/',
-      desc: 'TuneSet creates Traditional Irish abc music files, which include the name of the tune, the type of which it is a jig or a reel, the key, and other info about the tune. A page generates, which the abc music notated into musical printed material. Another page shows you dynamically key choices through TheSession.org\'s API and returns a list of tunes and the user can choose a tune, which generates another page that shows all of the tunes in their collection of tunes from a database.',
-      image: 'tuneSet.png',
-      icon: 'fa fa-music',
-      github: 'https://github.com/hillaryskye/tuneSet'
+      name: 'shopping-angular',
+      type: 'Galvanize Project',
+      url: 'https://shoppingangular.firebaseapp.com/',
+      desc: 'This app is a shopping cart for tea, which was developed using Angular and Firebase',
+      image: 'shopping-angular.png',
+      icon: 'fa fa-coffee',
+      github: 'https://github.com/hillaryskye/shopping-angular'
     },
     {
       name: 'reddit-firebase-angular',
+      type: 'Galvanize Project',
       url: 'https://redditfirebase.firebaseapp.com/',
       desc: 'Create, Read, Delete posts of a mock Reddit App with Firebase, Angular. Each post can be upvoted/ downvoted, favorited and you can add comments to each post.',
       image: 'reddit-firebase-angular.png',
@@ -20,20 +23,22 @@ app.controller("PortfolioCtrl", ["$scope", function($scope) {
       github: 'https://github.com/hillaryskye/reddit-firebase-angular'
     },
     {
+      name: 'tuneSet',
+      type: 'Personal Project',
+      url: 'https://stormy-waters-7608.herokuapp.com/',
+      desc: 'TuneSet creates Traditional Irish abc music files, which include the name of the tune, the type of which it is a jig or a reel, the key, and other info about the tune. A page generates, which the abc music notated into musical printed material. Another page shows you dynamically key choices through TheSession.org\'s API and returns a list of tunes and the user can choose a tune, which generates another page that shows all of the tunes in their collection of tunes from a database.',
+      image: 'tuneSet.png',
+      icon: 'fa fa-music',
+      github: 'https://github.com/hillaryskye/tuneSet'
+    },
+    {
       name: 'mixer',
+      type: 'Hackathon inspired Project',
       url: 'https://fathomless-wave-8517.herokuapp.com/colors',
-      desc: 'This app has 2 ways of creating a colors for use in developing a website. Firstly, you can just enter the red, green, blue values and secondly, you can choose a first color, a second color and it mixes a new color. All colors are saved for use in creating more colors.  This is a work in progress. I am still working on making it better and hoping to get some D3 functionality into it.',
+      desc: 'This app has 2 ways of creating a colors. Firstly, you can just enter the red, green, blue values and secondly, you can choose two colors and it mixes a new color and all colors are saved in a mongo database.',
       image: 'mixer.png',
       icon: 'fa fa-paint-brush',
       github: 'https://github.com/hillaryskye/mixer'
-    },
-    {
-      name: 'shopping-angular',
-      url: 'https://shoppingangular.firebaseapp.com/',
-      desc: 'This app is a shopping cart for tea, which was developed using Angular and Firebase',
-      image: 'shopping-angular.png',
-      icon: 'fa fa-coffee',
-      github: 'https://github.com/hillaryskye/shopping-angular'
     }
   ]
   $scope.sayings = ['Creative', 'Intelligent', 'Fun', 'Tenacity']
