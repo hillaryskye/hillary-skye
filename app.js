d647e865fa1a8c228a6a27a2ea0391e0a6d96308
Hillary Skye <skyeinc9@gmail.com>
2019-02-24 16:35:38 -0700

    added bootstrap with grunt and styling

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+// app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+// app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.use(express.static(path.join(__dirname + '/public')));
 
