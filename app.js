365c35ab81de30020d056a71f00668f1641056b0
Hillary Skye <skyeinc9@gmail.com>
2019-02-25 23:16:29 -0700

    styling with bootstrap4

--- app.js
+++ app.js
@@ -14,19 +14,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-// app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-// app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.use(function(req, res) {
   res.sendFile(__dirname + '/index.html');
+  console.log(req);
+  console.log(res);
 });
 
 app.listen(process.env.PORT || 3000, function(){
    console.log('listening port 3000');
 });
-
 
 module.exports = app;
--- public/js/directives.js
+++ public/js/directives.js
@@ -4,29 +4,18 @@ app.directive('scrollPosition', function($window) {
         scroll: '=scrollPosition'
       },
       link: function(scope, element, attrs) {
-        // var windowEl = angular.element($window);
-        // var handler = function() {
-        //   scope.scroll = windowEl.scrollTop();
-        //   console.log('scroll', scope.scroll)
-        // }
-        // windowEl.on('scroll', scope.$apply.bind(scope, handler));
-        // handler();
-        // console.log('scope', scope)
-        // console.log('handler', handler)
 
          //detect scroll
         $(window).scroll(function (event) {
             var scroll = $(window).scrollTop();
             scope.scroll = scroll;
-        console.log('scroll', scroll);
-        console.log('scope', scope)
         if(scroll>500 || scroll==undefined){
             scope.showUpArrow = false;
         }else{
             scope.showUpArrow = true;
         }
         scope.$apply();
-});
+        });
       }
     };
   });
